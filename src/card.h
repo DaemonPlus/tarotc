fffd7106120decf23f4dff8ddb09d9eeb99aefe9
oscarcederberg <os7138ce-s@student.lu.se>
2022-01-13 20:14:47 +0100

    Add comments to header

--- src/card.h
+++ src/card.h
@@ -3,10 +3,17 @@
 
 #include<stdlib.h>
 
+// Signifies an Arcana order. 
+// Is either MAJOR or MINOR.
 typedef enum arcana_order{MAJOR, MINOR} arcana_order;
+// Signifies the Suit of a card of Minor Arcana.
+// Is either STAFFS, CUPTS, SWORDS, PENTACLES, or NONE if not of Minor Arcana.
 typedef enum minor_suit{NONE, STAFFS, CUPS, SWORDS, PENTACLES} minor_suit;
+// Possible orientations of a card.
+// Is either UPRIGHT or REVERSE.
 typedef enum orientation{UPRIGHT, REVERSE} orientation;
 
+// The card structure type.
 typedef struct card_t{
     arcana_order    order;
     int             value;
@@ -15,8 +22,11 @@ typedef struct card_t{
     orientation     orientation;
 } card_t;
 
+// Creates a card with correct name in upright position.
+// Returns a pointer to the card_t struct.
 card_t* create_card(arcana_order order, int value, minor_suit suit);
-card_t* name_card(card_t* card);
+
+// Frees a card struct and its members from meory.
 void free_card(card_t* card);
 
 #endif

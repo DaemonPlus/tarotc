9d529504399d12852dfdb3f90bd0f877785913d9
oscarcederberg <os7138ce-s@student.lu.se>
2022-01-15 18:44:09 +0100

    Name convention

--- src/card.c
+++ src/card.c
@@ -1,23 +1,35 @@
-#include<card.h>
+#define str(x) #x
+#define xstr(x) str(x)
+
+#include <card.h>
+#include <stdio.h>
+
 void name_card(card_t* card);
 
-card_t* create_card(arcana_order order, int value, minor_suit suit){
+card_t* create_card(order_t order, int value, suit_t suit){
     card_t* card;
     
     card = (card_t*) calloc(1, sizeof(card));
     card->order = order;
     card->value = value;
     card->suit = suit;
+    card->orientation = REVERSE;
     name_card(card);
 }
 
 void name_card(card_t* card){
+    size_t length;
+
     switch(card->order){
         case MAJOR:
-
+            length = snprintf(NULL, 0, "%d - unkown", card->value);
+            card->name = malloc(length * sizeof(char));
+            snprintf(card->name, length, "%d - unkown", card->value);
             break;
         case MINOR:
-            
+            length = snprintf(NULL, 0, "%d of %s", card->value, card->value, xstr(card->suit));
+            card->name = malloc(length * sizeof(char));
+            snprintf(card->name, length, "%d of %s", card->value, card->value, xstr(card->suit));
             break;
     }
 }
--- src/card.h
+++ src/card.h
@@ -1,32 +1,30 @@
 #ifndef CARD_H
 #define CARD_H
 
-#include<stdlib.h>
-
 // Signifies an Arcana order. 
 // Is either MAJOR or MINOR.
-typedef enum arcana_order{MAJOR, MINOR} arcana_order;
+typedef enum {MAJOR, MINOR} order_t;
 // Signifies the Suit of a card of Minor Arcana.
 // Is either STAFFS, CUPTS, SWORDS, PENTACLES, or NONE if not of Minor Arcana.
-typedef enum minor_suit{NONE, STAFFS, CUPS, SWORDS, PENTACLES} minor_suit;
+typedef enum {NONE, STAFFS, CUPS, SWORDS, PENTACLES} suit_t;
 // Possible orientations of a card.
 // Is either UPRIGHT or REVERSE.
-typedef enum orientation{UPRIGHT, REVERSE} orientation;
+typedef enum {UPRIGHT, REVERSE} orientation_t;
 
 // The card structure type.
 typedef struct card_t{
-    arcana_order    order;
+    order_t         order;
     int             value;
-    minor_suit      suit;
+    suit_t          suit;
     char*           name;
-    orientation     orientation;
+    orientation_t   orientation;
 } card_t;
 
 // Creates a card with correct name in upright position.
 // Returns a pointer to the card_t struct.
-card_t* create_card(arcana_order order, int value, minor_suit suit);
+card_t* create_card(order_t order, int value, suit_t suit);
 
-// Frees a card struct and its members from meory.
+// Frees a card struct and its members.
 void free_card(card_t* card);
 
 #endif
